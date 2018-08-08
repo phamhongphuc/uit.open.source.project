372296c52f230b73533fded7ea034ca7986e2940
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-08-08 17:34:48 +0700

    Run type:fix

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
-const express = require('express')
+const express = require('express');
 
 // Create express instnace
-const app = express()
+const app = express();
 
 // Require API routes
-const users = require('./routes/users')
+const users = require('./routes/users');
 
 // Import API Routes
-app.use(users)
+app.use(users);
 
 // Export the server middleware
 module.exports = {
-  path: '/api',
-  handler: app
-}
+    path: '/api',
+    handler: app,
+};
