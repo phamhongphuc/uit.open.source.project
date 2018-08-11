c964f5166e3107ba39ff1f743f489300bef6b264
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-08-11 11:27:14 +0700

    Package: Use nuxt-edge

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,43 +1,37 @@
-module.exports = {
-    /*
-  ** Headers of the page
-  */
+import bodyParser from 'body-parser';
+import session from 'express-session';
+
+export default {
     head: {
-        title: 'starter',
+        title: 'Auth Routes',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+            { hid: 'description', content: 'Auth Routes example' },
         ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
     },
-    /*
-  ** Global CSS
-  */
     css: ['~/assets/css/main.css'],
-    /*
-  ** Add axios globally
-  */
     build: {
-        vendor: ['axios'],
-        /*
-    ** Run ESLINT on save
-    */
+        watch: ['api'],
         extend(config, ctx) {
             if (ctx.isDev && ctx.isClient) {
                 config.module.rules.push({
                     enforce: 'pre',
                     test: /\.(js|vue)$/,
                     loader: 'eslint-loader',
-                    exclude: /(node_modules)/
+                    exclude: /(node_modules)/,
                 });
             }
-        }
+        },
     },
     serverMiddleware: [
-    // API middleware
-        '~/api/index.js'
-    ]
+        bodyParser.json(),
+        session({
+            secret: 'super-secret-key',
+            resave: false,
+            saveUninitialized: false,
+            cookie: { maxAge: 60000 },
+        }),
+        '~/api',
+    ],
 };
