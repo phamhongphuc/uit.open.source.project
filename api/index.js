e17de4061f234b0cc3acab90e6a4d9abec8b84f6
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-08-18 10:21:37 +0700

    Use koa instead of express

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
-import express from 'express';
+import koa from 'koa';
+import cors from '@koa/cors';
+
 import users from './routes/users';
 
-const app = express();
+const app = new koa();
 
-app.use(users);
+app.use(
+    cors({
+        origin: 'http://localhost:8080',
+    }),
+);
 
-export default {
-    path: '/api',
-    handler: app,
-};
+app.use(users.routes());
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port 3000!');
+});
