bc86e3d056cbec26e5152e4330cf03a9014c9dd4
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-11-11 21:45:56 +0700

    Update Layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,17 +1,21 @@
 <template>
     <section class="container">
-        <img src="../assets/img/logo.png" 
-             alt="Nuxt.js Logo" 
-             class="logo" >
+        <img
+            src="../assets/img/logo.png"
+            alt="Nuxt.js Logo"
+            class="logo"
+        >
         <h1 class="title">
             {{ error.statusCode }}
         </h1>
         <h2 class="info">
             {{ error.message }}
         </h2>
-        <nuxt-link v-if="error.statusCode === 404" 
-                   class="button" 
-                   to="/">
+        <nuxt-link
+            v-if="error.statusCode === 404"
+            class="button"
+            to="/"
+        >
             Homepage
         </nuxt-link>
     </section>
@@ -32,11 +36,13 @@ export default {
     margin-top: 15px;
     font-size: 5em;
 }
+
 .info {
     font-weight: 300;
     color: #9aabb1;
     margin: 0;
 }
+
 .button {
     margin-top: 50px;
 }
