07d55f7f51e304d9b0f3883a1def564640d25f43
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-11-11 21:37:54 +0700

    Update plugins/axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const options = {};
 
-const PORT = 3000; // process.env.NODE_ENV == 'development' ? 3000 : process.env.PORT || 8080;
 const HOST = process.env.HOST || 'localhost';
+const PORT =
+    process.env.NODE_ENV === 'development' ? 3000 : process.env.PORT || 8080;
 
 options.baseURL = `http://${HOST}:${PORT}`;
 
