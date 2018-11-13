93050a025201f74b8dcd32cba461c513fb8e3cf0
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-11-13 22:41:22 +0700

    Update `plugin/axios.js`

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-const options = {};
-
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.NODE_ENV === 'development' ? 3000 : process.env.PORT || 8080;
-
-options.baseURL = `http://${HOST}:${PORT}`;
-
-export default axios.create(options);
+export default axios.create({
+    baseURL: process.env.SERVER_API,
+});
