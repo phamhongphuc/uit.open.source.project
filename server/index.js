f18f4da226dc22d71b3720c083432635b9e496b5
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-11-16 14:26:36 +0700

    Update `index.js`

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 3000;
         nuxt.render(ctx.req, ctx.res);
     });
 
-    const server = app.listen(PORT, HOST, async () => {
+    app.listen(PORT, HOST, async () => {
         console.log(`Server is running on port ${PORT}!`);
 
         if (process.env.NODE_ENV === 'generation') {
