a7eb2cdda2d4617737c95ac0d40f21ec2fb16827
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-11-21 10:02:07 +0700

    Remove carousel, navbar from default layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,21 +1,15 @@
 <template>
     <div>
-        <navbar- />
-        <carousel- />
         <nuxt />
         <footer- />
     </div>
 </template>
 
 <script>
-import carousel from '~/components/header/carousel.vue';
-import navbar from '~/components/header/navbar.vue';
 import footer from '~/components/footer/footer.vue';
 
 export default {
     components: {
-        'carousel-': carousel,
-        'navbar-': navbar,
         'footer-': footer,
     },
 };
