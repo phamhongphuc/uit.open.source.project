826bbf524eda37d7795c0aeb3ae603a9599db2e5
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-11-22 21:47:06 +0700

    Add Image.vue

---
+++ client/components/utilities/image.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    props: {
+        source: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            isError: false,
+        };
+    },
+    methods: {
+        onError() {
+            this.isError = true;
+        },
+    },
+    render(createElement) {
+        return createElement('img', {
+            attrs: {
+                src: this.isError ? '/img/faq.svg' : this.source,
+            },
+            on: {
+                error: this.onError,
+            },
+        });
+    },
+};
+</script>
