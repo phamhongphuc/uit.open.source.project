95a8a92081f76de12308069d77c05fb0385bae8e
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-11-25 00:14:07 +0700

    Update image.vue

--- client/components/utilities/image.vue
+++ client/components/utilities/image.vue
@@ -5,10 +5,15 @@ export default {
             type: String,
             required: true,
         },
+        default: {
+            type: String,
+            default: '/img/circle.svg',
+        },
     },
     data() {
         return {
             isError: false,
+            isLoad: false,
         };
     },
     methods: {
@@ -19,10 +24,18 @@ export default {
     render(createElement) {
         return createElement('img', {
             attrs: {
-                src: this.isError ? '/img/faq.svg' : this.source,
+                src: this.isError ? this.default : this.source,
+            },
+            class: {
+                image: true,
+                'd-none': !this.isLoad,
+                'd-block': this.isLoad,
             },
             on: {
                 error: this.onError,
+                load: event => {
+                    this.isLoad = true;
+                },
             },
         });
     },
