fe7cf05674bd77809a21362b732197ae47a1d058
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-11-25 15:04:01 +0700

    Add dev api

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,15 +1,21 @@
 <template>
     <div id="body" class="container mt-3 mt-sm-4 mt-md-5">
-        <div class="row">
-        </div>
+        <div class="row" />
     </div>
 </template>
 <script>
+import axios from '~/plugins/axios';
+
 export default {
     components: {},
     head() {
         return {
             title: 'Ai Scanlation',
+        };
+    },
+    async asyncData() {
+        return {
+            items: (await axios.get('api/dev/mangas')).data,
         };
     },
 };
