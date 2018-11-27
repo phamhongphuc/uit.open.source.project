1b6fc5d593f7c76f991473025da81c112515ac8a
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-11-27 19:38:49 +0700

    Add ratio-box, book-item

---
+++ client/components/utilities/ratio-box.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="ratio-box" :style="{ paddingBottom: `${100 * (dy / dx)}%` }">
+        <div :class="childClass"><slot /></div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        dx: {
+            type: Number,
+            default: 1,
+            validator: value => value !== 0,
+        },
+        dy: {
+            type: Number,
+            default: 1,
+        },
+        childClass: {
+            type: [String, Object, Array],
+            default: '',
+        },
+    },
+};
+</script>
+<style lang="scss">
+.ratio-box {
+    width: 100%;
+    position: relative;
+    > * {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+}
+</style>
