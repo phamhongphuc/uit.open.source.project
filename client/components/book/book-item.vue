46277f613750b413a5d73fc7563192a30aedd2fb
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-11-29 14:45:15 +0700

    Update book-item grid

--- client/components/book/book-item.vue
+++ client/components/book/book-item.vue
@@ -1,47 +1,77 @@
 <template>
-    <ratio-box- class="book-item" :dx="16" :dy="9" multi-child>
-        <div class="right" />
-        <div class="left">Image</div>
-        <div class="right-text">Text</div>
+    <ratio-box- class="book-item" :dx="16" :dy="9">
+        <div class="book-item-background" />
+        <div class="book-item-image">
+            <image- redirect :source="item.img" />
+        </div>
+        <div class="book-item-text">Text</div>
     </ratio-box->
 </template>
 <script>
 export default {
     components: {
         ...'~/components/utilities/ratio-box.vue',
+        ...'~/components/utilities/image.vue',
+    },
+    props: {
+        item: {
+            type: Object,
+            default: () => ({
+                title: 'hello',
+                img: 'DrMpC5CXcAApzT7',
+            }),
+        },
     },
 };
 </script>
 <style lang="scss">
 .book-item {
-    box-shadow: $box-shadow-sm;
-    background: $white;
+    $card-w: 16;
+    $card-h: 9;
 
-    $card-x: 16;
-    $card-y: 9;
-
-    $book-x: 5;
-    $book-y: 7;
+    $wp: 1 / $card-w * 100%;
+    $hp: 1 / $card-h * 100%;
 
     $space: 1;
-    $space-x: 1/16;
-    $space-y: $space-x * $card-x / $card-y;
 
-    $left-width: #{$book-x / $book-y * $card-y / $card-x * (1 - $space-y) * 100%};
+    $image-r-w: 85; /* ratio */
+    $image-r-h: 120; /* ratio */
 
-    .left {
-        bottom: $space / $card-y * 100%;
-        background: red;
-        width: $left-width;
+    $image-h: $card-h - $space;
+    $image-w: $image-h / $image-r-h * $image-r-w;
+
+    $space-w: $space * $wp;
+    $space-h: $space * $hp;
+
+    > div {
+        display: grid;
+        grid-template-columns: $space-w #{($image-w - $space) * $wp} auto;
+        grid-template-rows: $space-h auto $space-h;
     }
-    .right {
-        background: blue;
-        top: $space / $card-y * 100%;
-        left: $space / $card-x * 100%;
+
+    .book-item-background {
+        grid-column: 2 / 4;
+        grid-row: 2 / 4;
+        box-shadow: $box-shadow-sm;
+        background: $white;
+        border-radius: $border-radius;
     }
-    .right-text {
-        top: $space / $card-y * 100%;
-        left: $left-width;
+    .book-item-image {
+        grid-column: 1 / 3;
+        grid-row: 1 / 3;
+        box-shadow: $box-shadow-sm;
+        background: $white;
+        border-radius: $border-radius;
+        overflow: hidden;
+        > img {
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+        }
+    }
+    .book-item-text {
+        grid-column: 3 / 4;
+        grid-row: 2 / 4;
     }
 }
 </style>
