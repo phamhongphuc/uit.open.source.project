6fe8f3fdc40d5adb6bef3a90af45c1ec1feb2156
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-11-29 20:51:35 +0700

    Update Index

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,21 @@
 <template>
     <div id="body" class="container mt-3 mt-sm-4 mt-md-5">
         <div class="row">
-            <div v-for="(item, index) in items" :key="index" class="col-6 p-2">
-                <book-item- :item="item" />
+            <div class="main-left col-12 col-md-8">
+                <div class="row">
+                    <div
+                        v-for="(item, index) in items"
+                        :key="index"
+                        class="col-12 p-3"
+                    >
+                        <book-item- :item="item" />
+                    </div>
+                </div>
             </div>
+            <div
+                class="main-right col-12 col-md-4 bg-dribbble vh-100"
+                style="opacity: 0.2"
+            />
         </div>
     </div>
 </template>
