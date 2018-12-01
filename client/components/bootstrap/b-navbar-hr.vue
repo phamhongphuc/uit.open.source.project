bed5d1417511c2a4b135810606eac2949142155f
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-12-01 20:38:23 +0700

    Update b-navbar-hr.vue

--- client/components/bootstrap/b-navbar-hr.vue
+++ client/components/bootstrap/b-navbar-hr.vue
@@ -4,7 +4,7 @@
 <style lang="scss">
 .navbar-hr {
     width: calc(100% + #{map-get($spacers, 2) * 2});
-    background-color: $mix;
+    background-color: $border-color;
     height: 1.5px;
     display: block;
     margin: 0 -#{map-get($spacers, 2)};
