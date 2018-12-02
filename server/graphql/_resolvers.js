bff7d642ec78c0ab43f9896a0ee750b95905ad75
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-12-02 18:54:47 +0700

    Add scalar Date

--- server/graphql/_resolvers.js
+++ server/graphql/_resolvers.js
@@ -1,3 +1,7 @@
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
+import moment from 'moment';
+
 import {
     Mutation as genreMutation,
     Query as genreQuery,
@@ -7,8 +11,38 @@ export default {
     Query: {
         ...genreQuery,
     },
-
     Mutation: {
         ...genreMutation,
     },
+    Date: new GraphQLScalarType({
+        name: 'Date',
+        description: 'Date custom scalar type',
+
+        /**
+         * Parse value from client to Date
+         * @param {String} value value from client
+         * @return {Date}
+         */
+        parseValue(value) {
+            const date = moment(value, 'DD-MM-YYYY');
+            if (!date.isValid()) throw new Error('Lỗi định dạng');
+            return moment(value, 'DD-MM-YYYY').toDate();
+        },
+
+        /**
+         * Parse Date Object from server to String
+         * @param {Date} value
+         * @return {String}
+         */
+        serialize(value) {
+            return moment(value).format('DD-MM-YYYY');
+        },
+
+        parseLiteral(ast) {
+            if (ast.kind === Kind.STRING) {
+                return this.parseValue(ast.value);
+            }
+            return null;
+        },
+    }),
 };
