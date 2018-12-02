ecea6ab41232f8154fe7e919f35178a5fa665d44
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2018-12-02 10:22:15 +0700

    Update index: book-item, comment

---
+++ client/components/pages/index/book-items.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="row px-2">
+            <b-button
+                v-for="(set, index) in ['Recommended', 'Popular', 'Latest']"
+                :key="index"
+                class="m-2 shadow"
+                size="md"
+                :variant="select === set ? 'netlify' : 'white'"
+                @click="select = set"
+            >
+                {{ set }}
+            </b-button>
+        </div>
+        <div class="row px-2">
+            <div v-for="(item, index) in items" :key="index" class="col-12 p-2">
+                <book-item- :item="item" />
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    components: {
+        ...'~/components/book/book-item.vue',
+    },
+    data() {
+        return {
+            select: 'recommended',
+        };
+    },
+    computed: {
+        ...mapState({
+            items: state => state.book.items,
+        }),
+    },
+};
+</script>
+<style lang="scss">
+//@at-root
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,75 +1,25 @@
 <template>
     <div id="body" class="container">
         <div class="row">
-            <div class="main-left col-12 col-md-8 py-3">
-                <div class="row px-2">
-                    <b-button
-                        class="mx-2 shadow"
-                        size="md"
-                        :variant="
-                            select === 'recommended' ? 'netlify' : 'white'
-                        "
-                        @click="select = 'recommended'"
-                    >
-                        Recommended
-                    </b-button>
-                    <b-button
-                        class="mx-2 shadow"
-                        size="md"
-                        :variant="select === 'popular' ? 'netlify' : 'white'"
-                        @click="select = 'popular'"
-                    >
-                        Popular
-                    </b-button>
-                    <b-button
-                        class="mx-2 shadow"
-                        size="md"
-                        :variant="select === 'latest' ? 'netlify' : 'white'"
-                        @click="select = 'latest'"
-                    >
-                        Latest
-                    </b-button>
-                </div>
-                <div class="row p-2">
-                    <div
-                        v-for="(item, index) in items"
-                        :key="index"
-                        class="col-12 p-2"
-                    >
-                        <book-item- :item="item" />
-                    </div>
-                </div>
-            </div>
-            <div class="main-right col-12 col-md-4" />
+            <book-items- class="col-12 col-md-8 py-3" />
+            <comments- class="col-12 col-md-4 py-3" />
         </div>
     </div>
 </template>
 <script>
-import axios from '~/plugins/axios';
-
 export default {
     components: {
-        ...'~/components/book/book-item.vue',
+        ...'~/components/pages/index/book-items.vue',
+        ...'~/components/pages/index/comments.vue',
     },
     head() {
         return {
             title: 'Ai Scanlation',
         };
     },
-    data() {
-        return {
-            select: 'recommended',
-        };
-    },
-    async asyncData() {
-        return {
-            items: (await axios.get('api/dev/mangas')).data,
-        };
+    async asyncData({ store }) {
+        await store.dispatch('book/fetchItems');
+        return {};
     },
 };
 </script>
-<style lang="scss">
-.main-right {
-    background-color: rgba(black, 0.2);
-}
-</style>
